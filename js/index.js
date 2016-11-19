2627bf54b84e0be43b95ed78524565731f2ca8d4
code-lynn <lylcode@163.com>
2016-11-20 07:06:56 +0800

    2016_11_20

--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@
  */
 var mySwiper=new Swiper('.swiper-container',{
     direction:'vertical',
+    /*effect : 'cube',
+    cube: {
+        slideShadows: true,
+        shadow: true,
+        shadowOffset: 100,
+        shadowScale: 0.6
+    },*/
     //autoplay:2000,
     //autoplayDisableOnInteraction:false,
     loop:true,/*可循换-->因为loop之后swiper在首尾默认增加了两个滑块-要让首尾各添加的滑块显示正确的效果*/
@@ -18,7 +25,7 @@ var mySwiper=new Swiper('.swiper-container',{
                     case 0:
                         item.id='page'+trueIndexSlide;
                         break;
-                    case length-1:
+                    case (slides.length-1):
                         item.id='page1';
                         break;
                     default :
@@ -28,3 +35,24 @@ var mySwiper=new Swiper('.swiper-container',{
         })
     }
 });
+/*MUSIC*/
+var music = document.querySelector(".music"),
+    proudBoy = document.querySelector("#proudBoy");
+window.setTimeout(function(){
+    proudBoy.play();
+    proudBoy.addEventListener("canplay",function(){
+        music.className = "music musicCur";
+    })
+},1000);
+music.addEventListener("click",function(){
+    //paused属性判断音频文件是播放的还是停止 beyond.paused=true说明音频文件目前是停止播放
+    //让音频文件播放play(),让音频文件停止是pause();
+    if(proudBoy.paused){//停止
+        proudBoy.play();
+        music.className = "music musicCur";
+    }else{
+        proudBoy.pause();
+        music.className = "music";
+        music.style.opacity = 1;
+    }
+},false);
